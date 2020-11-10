6c370255d7bebfa35cb6e9ea1cb681ff9df49562
Jimmy Provencher <jimmy.provencher@cgi.com>
2020-11-10 02:24:09 -0500

    Adding provider to main VM class

--- libraries/VM.rb
+++ libraries/VM.rb
@@ -17,6 +17,10 @@
 # single word that starts with a capital letter and then continues to use
 # camel-casing throughout the remainder of the name.
 #
+
+require_relative "VBox/VBoxProvider"
+require_relative "VSphere/VSphereProvider"
+
 module Infraclass
   module VmHelpers
     #
