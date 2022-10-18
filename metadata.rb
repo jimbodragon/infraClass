2a161d00a9ee2bb80a6e318ba4ccded4f338bef1
Jimmy Provencher <jimmy.provencher@hotmail.ca>
2022-10-17 23:17:41 -0400

    Keeping metadata information for cookbook

--- metadata.rb
+++ metadata.rb
@@ -1,25 +1,25 @@
-# name 'infraClass'
-# maintainer 'Jimbo Dragon'
-# maintainer_email 'jimbo_dragon@hotmail.com'
-# license 'MIT'
-# description 'Installs/Configures infraClass'
-# version '0.1.1'
-# chef_version '>= 16.6.14'
+name 'infraClass'
+maintainer 'Jimbo Dragon'
+maintainer_email 'jimbo_dragon@hotmail.com'
+license 'MIT'
+description 'Installs/Configures infraClass'
+version '0.1.1'
+chef_version '>= 16.6.14'
 
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/jimbodragon/infraClass/issues'
+The `issues_url` points to the location where issues for this cookbook are
+tracked.  A `View Issues` link will be displayed on this cookbook's page when
+uploaded to a Supermarket.
 
-# The `source_url` points to the development repository for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/jimbodragon/infraClass'
+issues_url 'https://github.com/jimbodragon/infraClass/issues'
 
-# supports ubuntu
-# supports debian
+The `source_url` points to the development repository for this cookbook.  A
+`View Source` link will be displayed on this cookbook's page when uploaded to
+a Supermarket.
+
+source_url 'https://github.com/jimbodragon/infraClass'
+
+supports ubuntu
+supports debian
 
 # depends 'chef-git-server'
 # depends 'virtualbox'
